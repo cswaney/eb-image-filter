59ee7a4ce837500d5da92505799d09edca2cce8e
cswaney <colinswaney@gmail.com>
2019-09-09 12:02:04 -0400

    TODO 1
    Implemented filteredimage endpoint.

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import express, { Router, Request, Response } from 'express';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
 (async () => {
@@ -13,30 +14,37 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
-  // GET /filteredimage?image_url={{URL}}
-  // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
-  // QUERY PARAMATERS
-  //    image_url: URL of a publicly accessible image
-  // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
-
-  /**************************************************************************** */
-
-  //! END @TODO1
-  
   // Root Endpoint
   // Displays a simple message to the user
   app.get( "/", async ( req, res ) => {
     res.send("try GET /filteredimage?image_url={{}}")
   } );
   
+  // endpoint to filter an image from a public url.
+  // REQUEST
+  //   GET /filteredimage?image_url={{URL}}
+  // RESPONSE
+  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
+  app.get("/filteredimage/", (req: Request, res: Response) => {
+    let { image_url } = req.query;
+
+    // validate the image_url query
+    if ( !image_url ) {
+      return res.status(400)
+        .send(`image_url is required`);
+    }
+
+    // call filterImageFromURL(image_url) to filter the image (and return local image path)
+    filterImageFromURL(image_url).then(filteredpath => {
+      // send the resulting file in the response
+      return res.status(200)
+        .sendFile(filteredpath, () => {  // transfers the file at the given path
+          console.log('Sent:', filteredpath)
+          deleteLocalFiles([filteredpath])  // deletes any files on the server on finish of the response
+        })
+    })
+
+  });
 
   // Start the Server
   app.listen( port, () => {
